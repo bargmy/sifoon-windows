7af7d3d73f6715e4106f86ceb0145d49ae92ac9b
bargmy <bargmy@users.noreply.github.com>
2026-05-15 06:41:55 +0000

    Fix build errors by updating toolset to v143 and implement SNI Spoof mode with Win7 support

--- src/targetver.h
+++ src/targetver.h
@@ -26,6 +26,6 @@
 
 // We only support Windows Vista and later because we use inet_ntop and that is not supported
 // on lower versions.
-#define _WIN32_WINNT 0x0600
+#define _WIN32_WINNT 0x0601
 
 #include <SDKDDKVer.h>
